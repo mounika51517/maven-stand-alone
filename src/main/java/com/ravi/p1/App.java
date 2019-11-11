7a9cb59b9ada11a5b1eba41189859416326ee0c7
Cloud User <centos@ip-172-31-39-90.ec2.internal>
2019-11-11 08:34:15 +0000

    push1

--- src/main/java/com/ravi/p1/App.java
+++ src/main/java/com/ravi/p1/App.java
@@ -8,7 +8,7 @@ package com.ravi.p1;
 {
     public static void main( String[] args )
     {
-        System.out.println( "Working with Jenkins webhook trigger" );
+        System.out.println( "Working with Jenkins through push1" );
         System.out.println( "Testing Webhooks" );
     }
 }
