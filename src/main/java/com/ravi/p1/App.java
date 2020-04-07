5d0885e636bd44fbab8277f26e34b7a2771f3e41
mounika51517 <56929609+mounika51517@users.noreply.github.com>
2020-04-07 13:58:44 +0100

    Update App.java

--- src/main/java/com/ravi/p1/App.java
+++ src/main/java/com/ravi/p1/App.java
@@ -8,7 +8,7 @@ package com.ravi.p1;
 {
     public static void main( String[] args )
     {
-        System.out.println( "Working with Jenkins through push3" );
-        System.out.println( "Testing Webhooks master" );
+        System.out.println( "Working with Jenkins through push" );
+        System.out.println( "Testing Webhooks" );
     }
 }
